14a990dd2c1c56a43b4463fac9c5b89f04522bef
Felipe Genuino <design@felipegenuino.com>
2020-03-20 23:57:22 -0300

    Update metodo, ui-blockote and encontre-profissional

--- page-findProfessional.php
+++ page-findProfessional.php
@@ -11,30 +11,33 @@ get_header(); ?>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <article class="col-12">
 
-                <section > 
-                <div class="row"> 
-                    <div class="col-12">
-                        <h2 class="ui-title ui-title--bottom-border ui-title--center">
-                            <span class="ui-title__label">CONHEÇA</span>
-                            <span class="ui-title__text"> ENCONTRE UM PROFISSIONAL <br> 
-                                    <span class="fw-l">CERTIFICADO RTA</span>
-                            </span> 
-                        </h2> 
-                    </div>
-                </div> 
+                
+                <section> 
+                    <div class="container"> 
+                        <div class="row"> 
+                            <div class="col-12">
+                                <h2 class="ui-title ui-title--bottom-border ui-title--center">
+                                        <span class="ui-title__label">CONHEÇA</span>
+                                        <span class="ui-title__text"> ENCONTRE UM PROFISSIONAL <br> 
+                                        <span class="fw-l">CERTIFICADO RTA</span> 
+                                </h2> 
+                            </div>
+                        </div> 
+                    </div> <!-- // container -->
                 </section>
 
 
                 <section class="d-flex flex-column"> 
+                <div class="container"> 
                     <div class="row">
                         <div class="col-12 col-lg-5 ml-lg-auto "> 
-                            <div class="encontre-profissional_blockote"> 
+                            <blockquote class="ui-blockote"> 
                                 <p>Se você fez o curso e deseja que seu contato apareça na lista mande um e-mail com seus dados e cópia do certificado para rtaonline@gmail.com</p>
-                            </div>  
+                            </blockquote>  
                         </div> 
                     </div>
                     <div class="row">  
-                            <div class="col-12 col-lg-6 "> 
+                            <div class="col-12 col-lg-4 offset-lg-1  "> 
                                 <div class="form-group">
                                     <label for="inputSearch" class="sr-only sr-only-focusable">Pesquisar por cidade ou nome</label>
                                     <div class="ui-seach">
@@ -46,49 +49,50 @@ get_header(); ?>
                                 </div>  
                             </div>  
                     </div> <!-- //row -->
-                    <ul class="encontre-profissional_list">
-                        <li  class="encontre-profissional_item" 
-                        data-filter-item 
-                        data-filter-name="adalberto Oliveira" 
-                        data-filter-city="porto alegre"
-                         >
-                            <div class="encontre-profissional_item-name"  >  <span> Adalberto Oliveira Soares Junior </span>  </div>
-                            <div class="encontre-profissional_item-phone"> <span>(48) 99911-3048 </span> </div>
-                            <div class="encontre-profissional_item-email"> <span>email@email.com</span></div>
-                            <div class="encontre-profissional_item-state"> <span>Rio grande do sul</span></div>
-                            <div class="encontre-profissional_item-city"> <span>Porto Alegre</span> </div>
-                        </li>
-                        <li  class="encontre-profissional_item" 
-                        data-filter-item 
-                        data-filter-name="maria joana" 
-                        data-filter-city="curitiba"
-                        >
-                            <div class="encontre-profissional_item-name" >  <span> Maria joana </span>  </div>
-                            <div class="encontre-profissional_item-phone"> <span>(55) 99531026 </span> </div>
-                            <div class="encontre-profissional_item-email"> <span>maria@email.com</span></div>
-                            <div class="encontre-profissional_item-state"> <span>Paraná</span></div>
-                            <div class="encontre-profissional_item-city"> <span>Curitiba</span> </div>
-                        </li>
 
-                        <li 
-                        class="encontre-profissional_item" 
-                        data-filter-name="zurilda"  
-                         data-filter-item   
-                        data-filter-city="marilia" 
-                         >
-                            <div class="encontre-profissional_item-name">  <span> Zurilda </span>  </div>
-                            <div class="encontre-profissional_item-phone"> <span>(11) 99999-9999 </span> </div>
-                            <div class="encontre-profissional_item-email"> <span>zurilda@email.com</span></div>
-                            <div class="encontre-profissional_item-state"> <span>São Paulo</span></div>
-                            <div class="encontre-profissional_item-city"> <span>Marilia</span> </div>
-                        </li>
+                    <div class="row justify-content-lg-center">  
+                            <div class="col-12 col-lg-10 "> 
+                                    <ul class="encontre-profissional_list">
+                                        <li  class="encontre-profissional_item" 
+                                        data-filter-item 
+                                        data-filter-name="adalberto Oliveira" 
+                                        data-filter-city="porto alegre"
+                                        >
+                                            <div class="encontre-profissional_item-name"  >  <span> Adalberto Oliveira Soares Junior </span>  </div>
+                                            <div class="encontre-profissional_item-phone"> <span>(48) 99911-3048 </span> </div>
+                                            <div class="encontre-profissional_item-email"> <span>email@email.com</span></div>
+                                            <div class="encontre-profissional_item-state"> <span>Rio grande do sul</span></div>
+                                            <div class="encontre-profissional_item-city"> <span>Porto Alegre</span> </div>
+                                        </li>
+                                        <li  class="encontre-profissional_item" 
+                                        data-filter-item 
+                                        data-filter-name="maria joana" 
+                                        data-filter-city="curitiba"
+                                        >
+                                            <div class="encontre-profissional_item-name" >  <span> Maria joana </span>  </div>
+                                            <div class="encontre-profissional_item-phone"> <span>(55) 99531026 </span> </div>
+                                            <div class="encontre-profissional_item-email"> <span>maria@email.com</span></div>
+                                            <div class="encontre-profissional_item-state"> <span>Paraná</span></div>
+                                            <div class="encontre-profissional_item-city"> <span>Curitiba</span> </div>
+                                        </li>
 
-                        
+                                        <li 
+                                        class="encontre-profissional_item" 
+                                        data-filter-name="zurilda"  
+                                        data-filter-item   
+                                        data-filter-city="marilia" 
+                                        >
+                                            <div class="encontre-profissional_item-name">  <span> Zurilda </span>  </div>
+                                            <div class="encontre-profissional_item-phone"> <span>(11) 99999-9999 </span> </div>
+                                            <div class="encontre-profissional_item-email"> <span>zurilda@email.com</span></div>
+                                            <div class="encontre-profissional_item-state"> <span>São Paulo</span></div>
+                                            <div class="encontre-profissional_item-city"> <span>Marilia</span> </div>
+                                        </li> 
+                                    </ul>
+                       </div>  
+                    </div> <!-- //row -->
 
-                        
-                    </ul>
-                   
-                    
+                     </div> <!-- // container -->
                 </section>
                 
                     
--- page-methodRTA.php
+++ page-methodRTA.php
@@ -11,34 +11,40 @@ get_header(); ?>
 
 
 <section class="metodo  w-100 d-flex flex-column ">
-
-
-<div class="container">
-    <div class="row justify-content-lg-center">
-        <div class="col-lg-8 metodo_col">
-        
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
-                
-             
-
- <div class="col-12">
-                <h1 class="ui-title ui-title--bottom-border ui-title--center">
-                    <span class="ui-title__label">Método RTA</span>
-                    <span class="ui-title__text">  O Método Reequilibrio   <br> Toracoabdominal - RTA   </span> 
-                </h1> 
-                </div>
-
-          
-            
-                <?php the_content(); ?> 
-    
-        <?php endwhile; else: ?> 
-        <?php endif; ?>
-
-        </div> <!-- //col-12 -->
-    </div> <!-- //row --> 
-</div> <!-- //container -->
  
+<div class="container">
+
+
+<div class="row">
+    <div class="col-12"> 
+        <h1 class="ui-title ui-title--bottom-border ui-title--center">
+            <span class="ui-title__label">Método RTA</span>
+            <span class="ui-title__text">  O Método Reequilibrio   <br> Toracoabdominal - RTA   </span> 
+        </h1> 
+    </div> 
+</div>       
+
+<div class="row">
+    <div class="col-12 col-lg-11 mt-3 mb-4 "> 
+        <blockquote class="ui-blockote"> 
+            <p>O método Reequilíbrio Toracoabdominal – RTA é uma abordagem da fisioterapia que avalia e trata o sistema respiratório de maneira integral em todas as idades – bebês, crianças e adultos.</p>
+        </blockquote>  
+    </div> 
+</div>
+
+
+ 
+    <div class="row justify-content-lg-center">
+        <div class="col-12 col-lg-8 "> 
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>  
+                        <?php the_content(); ?>  
+                <?php endwhile; else: ?> 
+                <?php endif; ?>
+        </div>
+    </div> <!-- //row --> 
+
+
+    </div> <!-- //container --> 
 </section>
  
 

80a03ac809cb0750d23084410921ee9669264791
Felipe Genuino <design@felipegenuino.com>
2020-05-30 17:01:15 -0300

    seo update

--- header.php
+++ header.php
@@ -12,6 +12,37 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<title><?php bloginfo('name'); ?> <?php wp_title('|',true,'left'); ?>  </title>
+ 
+
+<link rel="icon" href="<?php bloginfo('template_url'); ?>/images/seo/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php bloginfo('template_url'); ?>/images/seo/apple-touch-icon-144x144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php bloginfo('template_url'); ?>/images/seo/apple-touch-icon-114x114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php bloginfo('template_url'); ?>/images/seo/apple-touch-icon-72x72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="<?php bloginfo('template_url'); ?>/images/seo/apple-touch-icon-precomposed.png">
+    
+
+<link rel="canonical" href="<?php echo esc_url( home_url( '/' ) ); ?>" />
+<meta name="description" content="O Método RTA pode ser aplicado a pacientes de todas as idades e patologias que resultem em disfunção respiratória, desde o prematuro até o adulto.">
+<meta name="keywords" content="metodo rta, rta, cuidado global, mariângela pinheiro de lima, fisioterapia, fisioterapia adulto, fisioterapia neonatal, fisioterapia pediatrica, rta online">
+<meta property="og:locale" content="pt_br" />
+<meta property="og:type" content="website">
+<meta property="og:title" content="O Cuidado Global começa com uma respiração tranquila - RTA" />
+<meta property="og:description" content="O Método RTA pode ser aplicado a pacientes de todas as idades e patologias que resultem em disfunção respiratória, desde o prematuro até o adulto." />
+<meta property="og:url" content="<?php echo esc_url( home_url( '/' ) ); ?>" />
+<meta property="og:site_name" content="RTA Online | O Cuidado Global começa com uma respiração tranquila" />
+
+<meta property="og:image" content="<?php bloginfo('template_url'); ?>/images/seo/facebook.jpg">
+<meta property="og:image:type" content="image/jpeg">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+
+<meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:site" content="<?php echo esc_url( home_url( '/' ) ); ?>"/>
+<meta name="twitter:domain" content="O Método RTA pode ser aplicado a pacientes de todas as idades e patologias que resultem em disfunção respiratória, desde o prematuro até o adulto."/>
+
+<meta name="twitter:image:src" content="<?php bloginfo('template_url'); ?>/images/seo/facebook.jpg"/>
+
 <?php wp_head(); ?>
 </head>
 
